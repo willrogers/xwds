bed72b971dfe551756091d4b7bb1e666fd832dd7
Will Rogers <wahrogers@gmail.com>
2024-11-20 20:13:31 +0000

    Updates

---
+++ component---src-pages-404-js-150bd8e14951e37f3eba.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkxwds=self.webpackChunkxwds||[]).push([[125],{3331:function(e,t,l){l.r(t);var n=l(6540),s=l(88),u=l(7528);t.default=()=>n.createElement(s.A,null,n.createElement(u.A,{title:"404: Not found"}),n.createElement("h1",null,"NOT FOUND"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-150bd8e14951e37f3eba.js.map
