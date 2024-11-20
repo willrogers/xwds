3cc77173f14a6fe7b8fae7cc9dab7704d58beac1
Will Rogers <wahrogers@gmail.com>
2024-11-20 17:42:04 +0000

    Update header to match wllrg.rs.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,83 +1,81 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 /* Copied to match main.html from the main site. */
 
-class Header extends React.Component {
-  public props: { parentUrl: string };
-  componentDidMount(): void {
+const Header = (props: { parentUrl: string }): JSX.Element => {
+  useEffect(() => {
+    /* Add external script. */
     const script = document.createElement("script");
-    script.src = this.props.parentUrl + "/js/menu.js";
+    script.src = props.parentUrl + "/js/menu.js";
     script.async = true;
     document.body.appendChild(script);
-  }
-  render(): JSX.Element {
-    return (
-      <div id="header">
-        <div id="menu">
-          <div id="logo">
-            <div id="logo-img">
-              <img
-                src={this.props.parentUrl + "/static/logo.png"}
-                alt="pattern"
-              />
-            </div>
-            <a id="name-logo" href={this.props.parentUrl + "/index.html"}>
-              Will Rogers
-            </a>
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+  return (
+    <div id="header">
+      <div id="menu">
+        <div id="logo">
+          <div id="logo-img">
+            <img src={props.parentUrl + "/static/logo.png"} alt="pattern" />
           </div>
-          <label id="toggle-label"></label>
-          <input
-            type="checkbox"
-            id="toggle-menu"
-            name="toggle-label"
-            defaultChecked
-          />
-          <nav>
-            <div id="menu-header">
-              <a href={this.props.parentUrl + "/index.html"}>WR</a>
-              <img
-                id="sidebar-logo"
-                src={this.props.parentUrl + "/static/logo.png"}
-                alt="pattern"
-              />
-            </div>
-            <ul className="nav-items">
-              <li className="header-nav-item">
-                <a href={this.props.parentUrl + "/index.html"}>Blog</a>
-              </li>
-              <li className="header-nav-item">
-                <a href={this.props.parentUrl + "/about.html"}>About</a>
-              </li>
-              <li className="header-nav-item">
-                <a href={this.props.parentUrl + "/links.html"}>Links</a>
-              </li>
-              <li className="header-nav-item">
-                <a href={this.props.parentUrl + "/xwds"}>Crosswords</a>
-              </li>
-              <li className="header-nav-item">
-                <a href="https://github.com/willrogers">
-                  <img
-                    className="header-nav-github"
-                    src={this.props.parentUrl + "/static/GitHub-Mark-64px.png"}
-                    alt="github"
-                  ></img>
-                </a>
-              </li>
-              <li className="header-nav-item">
-                <a href="https://bsky.app/profile/wrgrs.bsky.social">
-                  <img
-                    className="header-nav-bluesky"
-                    src={this.props.parentUrl + "/static/bluesky.svg"}
-                    alt="twitter"
-                  ></img>
-                </a>
-              </li>
-            </ul>
-          </nav>
+          <a id="name-logo" href={props.parentUrl + "/index.html"}>
+            Will Rogers
+          </a>
         </div>
+        <label id="toggle-label"></label>
+        <input
+          type="checkbox"
+          id="toggle-menu"
+          name="toggle-label"
+          defaultChecked
+        />
+        <nav>
+          <div id="menu-header">
+            <a href={props.parentUrl + "/index.html"}>WR</a>
+            <img
+              id="sidebar-logo"
+              src={props.parentUrl + "/static/logo.png"}
+              alt="pattern"
+            />
+          </div>
+          <ul className="nav-items">
+            <li className="header-nav-item">
+              <a href={props.parentUrl + "/index"}>Blog</a>
+            </li>
+            <li className="header-nav-item">
+              <a href={props.parentUrl + "/about"}>About</a>
+            </li>
+            <li className="header-nav-item">
+              <a href={props.parentUrl + "/links"}>Links</a>
+            </li>
+            <li className="header-nav-item">
+              <a href={props.parentUrl + "/xwds"}>Crosswords</a>
+            </li>
+            <li className="header-nav-item">
+              <a href="https://github.com/willrogers">
+                <img
+                  className="header-nav-github"
+                  src={props.parentUrl + "/static/GitHub-Mark-64px.png"}
+                  alt="github"
+                ></img>
+              </a>
+            </li>
+            <li className="header-nav-item">
+              <a href="https://bsky.app/profile/wrgrs.bsky.social">
+                <img
+                  className="header-nav-bluesky"
+                  src={props.parentUrl + "/static/bluesky.svg"}
+                  alt="twitter"
+                ></img>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Header;
