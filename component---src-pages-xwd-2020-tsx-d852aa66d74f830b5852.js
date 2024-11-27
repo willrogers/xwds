21747670c03819daccb17a1d7145991a8e1eefa3
Will Rogers <wahrogers@gmail.com>
2024-11-27 20:34:31 +0000

    Updates

---
+++ component---src-pages-xwd-2020-tsx-d852aa66d74f830b5852.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkxwds=self.webpackChunkxwds||[]).push([[841],{9406:function(e,s,a){a.r(s);var l=a(6540),r=a(4794),t=a(7956);s.default=()=>{const e=(0,r.useStaticQuery)("2631139263").allXwd2020Json.edges[0].node,s=e.title,a=e.preamble,u=e.across_size,c=e.down_size,n=e.black_squares,i=e.clues;return l.createElement(t.A,{title:s,preamble:a,acrossSize:u,downSize:c,blackSquares:n,rawClues:i})}}}]);
+//# sourceMappingURL=component---src-pages-xwd-2020-tsx-d852aa66d74f830b5852.js.map
---
+++ component---src-pages-xwd-2024-tsx-5f979f014e36eda06974.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkxwds=self.webpackChunkxwds||[]).push([[997],{8610:function(e,s,a){a.r(s);var l=a(6540),t=a(4794),r=a(4484);s.default=()=>{const e=(0,t.useStaticQuery)("542524241").allXwd2024Json.edges[0].node,s=e.title,a=e.preamble,u=e.across_size,c=e.down_size,n=e.black_squares,d=e.clues;return l.createElement(r.default,{title:s,preamble:a,acrossSize:u,downSize:c,blackSquares:n,rawClues:d})}}}]);
+//# sourceMappingURL=component---src-pages-xwd-2024-tsx-5f979f014e36eda06974.js.map
