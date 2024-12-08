90c775154819eceb24dff5a96e83f756d29812eb
Will Rogers <wahrogers@gmail.com>
2024-12-08 13:32:20 +0000

    Updates

---
+++ component---src-pages-xwd-2024-tsx-45317593fdd16599f368.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkxwds=self.webpackChunkxwds||[]).push([[997],{8610:function(e,s,a){a.r(s);var l=a(6540),r=a(4794),t=a(7956);s.default=()=>{const e=(0,r.useStaticQuery)("542524241").allXwd2024Json.edges[0].node,s=e.title,a=e.preamble,u=e.across_size,c=e.down_size,n=e.black_squares,i=e.clues;return l.createElement(t.A,{title:s,preamble:a,acrossSize:u,downSize:c,blackSquares:n,rawClues:i})}}}]);
+//# sourceMappingURL=component---src-pages-xwd-2024-tsx-45317593fdd16599f368.js.map
